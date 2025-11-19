420c35d7389a4b821cb8145acacd115bcff624be
Kelvin Omozokpia <kelvinjune2@gmail.com>
2025-11-19 15:17:24 +0100

    feat: update DeleteModal button styles for consistent appearance

--- frontend/lib/components/DeleteModal.tsx
+++ frontend/lib/components/DeleteModal.tsx
@@ -98,6 +98,7 @@ export default function DeleteModal({
               type="submit"
               disabled={isSubmitting}
               loading={isSubmitting}
+              className="bg-red-400 hover:bg-red-400"
             >
               Delete
             </Button>
